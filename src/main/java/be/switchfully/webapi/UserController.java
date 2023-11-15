8acf16d0ce011dbe8700ab0d015756add6501fff
Laurent Herry <eradikalien@gmail.com>
2023-11-15 15:13:43 +0100

    itemGroup validator

--- src/main/java/be/switchfully/webapi/UserController.java
+++ src/main/java/be/switchfully/webapi/UserController.java
@@ -9,6 +9,7 @@ import jakarta.annotation.security.RolesAllowed;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 
 import java.util.UUID;
@@ -29,6 +30,11 @@ public class UserController {
 
     @GET
     @RolesAllowed("admin")
+    @Operation(
+            operationId = "getAllUsers",
+            summary = "All registered users",
+            description = "Allows the admin to see registered member."
+    )
     public Response getAllUsers() {
         return Response.status(OK).entity(userService.getAllUsers()).build();
     }
@@ -36,6 +42,11 @@ public class UserController {
     @GET
     @Path("/{id}")
     @RolesAllowed("admin")
+    @Operation(
+            operationId = "getUserById",
+            summary = "Get a user by ID",
+            description = "Allows the admin to have an overview over a member, based on the member's ID."
+    )
     public Response getUserById(@PathParam("id") UUID id) {
         return Response.status(OK).entity(userService.getUserById(id)).build();
     }
@@ -43,6 +54,11 @@ public class UserController {
     @GET
     @Path("/myOrder")
     @RolesAllowed("member")
+    @Operation(
+            operationId = "getAllOrder",
+            summary = "Get all my order",
+            description = "Allows the connected member to have a review of all his orders."
+    )
     public Response getAllOrders() {
         return Response.status(OK).entity(orderService.getAllOrders()).build();
     }
@@ -50,6 +66,11 @@ public class UserController {
     @POST
     @Path("/register")
     @PermitAll
+    @Operation(
+            operationId = "addUser",
+            summary = "Create a new user",
+            description = "Allows the user to create a member account."
+    )
     public Response addUser(CreateUserDTO createUserDTO) {
         return Response.status(CREATED).entity(userService.addUser(createUserDTO)).build();
     }
@@ -57,6 +78,11 @@ public class UserController {
     @POST
     @Path("/order")
     @RolesAllowed("member")
+    @Operation(
+            operationId = "addOrder",
+            summary = "Create an order",
+            description = "Create an order by giving the ID of the item and the desired amount."
+    )
     public Response addOrder(CreateOrderDTO createOrderDTO) {
         return Response.status(CREATED).entity(orderService.addOrder(createOrderDTO)).build();
     }
@@ -64,6 +90,11 @@ public class UserController {
     @POST
     @Path("/reorder/{id}")
     @RolesAllowed("member")
+    @Operation(
+            operationId = "addReOrder",
+            summary = "Order an already existing order(by ID)",
+            description = "Allows a connected member to order one of his existing order by providing the ID."
+    )
     public Response addReOrder(@PathParam("id") Long id) {
         return Response.status(CREATED).entity(orderService.addReOrder(id)).build();
     }
